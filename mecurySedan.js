f42a65371bd45745fa990997e744b74211a117c7
Dimario Oliver <Dimario Oliver>
2023-01-31 21:46:57 -0500

    imported fjs file into js file

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,8 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle.js")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+import { Vehicle } from "./vehicle.js";
